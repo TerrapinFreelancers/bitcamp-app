c35c58b79896dd8b887ef5fd7ce89c3b727be494
George <gtong@terpmail.umd.edu>
2016-09-11 18:07:16 -0400

    Deletes a bunch of commented out lines of useless  code I neglected to delete.

--- index.ios.js
+++ index.ios.js
@@ -18,20 +18,6 @@ class BitcampApp extends Component {
   render() {
     return (
       <MenuTab />
-
-
-      // <View style={styles.container}>
-      //   <Text style={styles.welcome}>
-      //     Welcome to React Native!
-      //   </Text>
-      //   <Text style={styles.instructions}>
-      //     To get started, edit index.ios.js
-      //   </Text>
-      //   <Text style={styles.instructions}>
-      //     Press Cmd+R to reload,{'\n'}
-      //     Cmd+D or shake for dev menu
-      //   </Text>
-      // </View>
     );
   }
 }
