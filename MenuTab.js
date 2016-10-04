19075cb8233e5b82d1c8252e8272ccf6c607ac75
Jack <jdai88@gmail.com>
2016-10-04 00:39:03 -0400

    Intro page (#17)
    * Add Intro scene, Aleo font

--- MenuTab.js
+++ MenuTab.js
@@ -5,6 +5,7 @@ import ScheduleScene from './scenes/ScheduleScene';
 import CompanyInfoScene from './scenes/CompanyInfoScene';
 import MapScene from './scenes/MapScene';
 import MentorsScene from './scenes/MentorsScene';
+import IntroScene from './scenes/IntroScene';
 
 //credit to/documentation at
 //https://github.com/skv-headless/react-native-scrollable-tab-view
@@ -24,6 +25,7 @@ export default class MenuTab extends Component {
 
     return (
       <ScrollableTabView tabBarPosition={menuTabPosition}>
+        <IntroScene tabLabel="Intro"/>
         <ScheduleScene tabLabel="Schedule" />
         <MapScene tabLabel="Map" />
         <CompanyInfoScene tabLabel="Company Info" />
