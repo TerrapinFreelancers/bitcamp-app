df14d490b4acbc3efa4427b83c0bcf36c8700858
AyeAndy <msoohyun88@gmail.com>
2017-01-11 04:27:42 +0900

    created new schedule view using accordion library

--- scenes/ScheduleScene.js
+++ scenes/ScheduleScene.js
@@ -1,7 +1,102 @@
 import React, { Component } from 'react';
-import { Animated,TouchableHighlight, ScrollView, View, Text } from 'react-native';
+import { AppRegistry, View, Text, ListView, StyleSheet } from 'react-native';
+import Accordion from 'react-native-accordion';
+
+class AccordionMenu extends Component {
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+        dataSource: ds.cloneWithRows(this._genRows({})),
+    };
+  }
+ 
+  render() {
+    console.log(this.state.dataSource);
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this._renderRow}
+      />
+    );
+  }
+ 
+  _renderRow(rowData) {
+
+    var header = (
+      <View style={styles.header}>
+        <Text>
+          <Text> {rowData[0]}     </Text>
+          <Text>{rowData[1]}</Text>
+        </Text>
+      </View>
+    );
+
+    var content = <View></View>;
+
+    //if the header is just a DATE STAMP, then create a new kind of date header
+    if(rowData[0] == "DATEHEADER"){
+      header = (
+        <View style={styles.date_header}>
+          <Text style={{color: '#ffffff', fontWeight: 'bold'}}>{rowData[1]}</Text>
+        </View>
+      );
+
+      return (
+      <Accordion
+        header={header}
+        content={content}
+        activeOpacity={0}
+        underlayColor={'#ffaf3f'}
+        easing="easeOutCubic"
+      />
+    );
+    }
+      
+    content = (
+        <View style={{
+          backgroundColor: '#eeeeee'
+        }}>
+          <Text style={styles.content}>{rowData[2]}</Text>
+          <Text style={styles.content}>Company: {rowData[3]}</Text>
+          <Text style={styles.content}>Location: {rowData[4]}</Text>
+        </View>
+    );
+
+    // console.log('returning', toRet);
+    return (
+      <Accordion
+        header={header}
+        content={content}
+        easing="easeOutCubic"
+      />
+    );
+  }
+
+  _genRows() {
+    //these are the data needed for schedule information.
+    var dataBlob = [];
+
+    //array to modify in firebase [event time, event name, event description, event company, event location]
+    var scheduleInfo = 
+    [["DATEHEADER", "April xxth"],
+    ["00:00 AM", "Intro", "introducing blah blah", "FINRA", "hi"], 
+    ["00:00 PM", "Next One", "no one cares blah", "Google", "hello"],
+    ["DATEHEADER", "April xxth"],
+    ["00:00 PM", "Last", "generic content lol", "MITRE", "sohai"]];
+
+    //arbitrary data blobs for accordion menu's sections
+    for(var i = 0; i < scheduleInfo.length; i++){
+      dataBlob.push(scheduleInfo[i]);
+    }
+
+    return dataBlob;
+  }
+  
+}
 
 export default class ScheduleScene extends Component {
+  
   static get defaultProps() {
     return {
       title: 'Schedule'
@@ -10,115 +105,38 @@ export default class ScheduleScene extends Component {
 
   render() {
     return (
-      <ScrollView>
-        <ScheduleElement eventName="Test1" eventTime={new Date()}
-          eventInfo = "lsjl eljk  dmm,xnv iowe k lsf wl ksjdfjio"/>
-        <ScheduleElement eventName="Test2" eventTime={new Date()}
-          eventInfo = "alsjljio elkj ew lksd kmx,cvo wlks djweio"/>
-        <ScheduleElement eventName="Test3" eventTime={new Date()}
-          eventInfo = "something alkjd adlkajfadkdkkflle ldjsf oiwe"/>
-      </ScrollView>
-    )
-  }
-};
-
-class ScheduleElement extends Component{
-
-  constructor(props){
-    super(props);
-    this.icons = {
-      up: "UP",
-      down: "DOWN"
-    };
-    this.elementHeight = 50;
-    this.state = {
-      expanded: true,
-      animation: new Animated.Value()
-    };
-  }
-
-  static get defaultProps() {
-    return {
-      eventName: "N/A",
-      eventTime: new Date(),
-      eventInfo: ""
-    };
-  }
-
-  toggle(){
-    let initialValue, finalValue;
-    if (this.state.expanded){
-      initialValue = this.state.maxHeight + this.state.minHeight;
-      finalValue = this.state.minHeight;
-    }else{
-      finalValue = this.state.maxHeight + this.state.minHeight;
-      initialValue = this.state.minHeight;
-    }
-    this.setState({
-      expanded: !this.state.expanded
-    });
-
-    this.state.animation.setValue(initialValue);
-    Animated.spring(
-      this.state.animation,
-      {
-        toValue: finalValue
-      }
-    ).start();
-
-
-  }
-
-  setMaxHeight(event){
-    this.setState({
-        maxHeight: event.nativeEvent.layout.height
-    });
-  }
-
-  setMinHeight(event){
-    this.setState({
-        minHeight: event.nativeEvent.layout.height
-    });
-  }
-
-  render(){
-    let icon = this.icons.down;
-    if (this.state.expanded){
-      icon = this.icons.up;
-    }
-    return (
-      <Animated.View style={{height:this.state.animation, overflow:'hidden'}}>
-        <TouchableHighlight onPress={this.toggle.bind(this)}>
-          <View style={{flex: 1, flexDirection: 'row', height: this.elementHeight,
-                        backgroundColor: 'powderblue', borderColor:'black',
-                        borderStyle: 'solid', borderWidth: 1, alignItems: 'center'}}
-                        onLayout={this.setMinHeight.bind(this)}>
-              <View style={{flex:1}}>
-                <Text>{this.props.eventTime.getMonth()}/
-                {this.props.eventTime.getDate()} {this.props.eventTime.getHours()}:
-                {this.props.eventTime.getMinutes()}
-                </Text>
-              </View>
-              <View style={{flex:1}}>
-                <Text style={{textAlign: "center"}}>{this.props.eventName}
-                </Text>
-              </View>
-              <View style={{flex:1}}>
-                <Text style={{textAlign: "right"}}>{icon}
-                </Text>
-              </View>
-          </View>
-        </TouchableHighlight>
-        <View onLayout={this.setMaxHeight.bind(this)}>
-          <Text>{this.props.eventInfo}</Text>
-        </View>
-      </Animated.View>
+      <AccordionMenu/>
     );
   }
-};
+}
 
-ScheduleElement.propTypes = {
-  eventName: React.PropTypes.string.isRequired,
-  eventTime: React.PropTypes.object.isRequired,
-  eventInfo: React.PropTypes.string
-};
+const styles = StyleSheet.create({
+  header: {
+        paddingTop: 20,
+        paddingRight: 15,
+        paddingLeft: 15,
+        paddingBottom: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#a9a9a9',
+        backgroundColor: '#e5d8ce'
+  },
+  date_header: {
+       paddingTop: 10,
+        paddingRight: 15,
+        paddingLeft: 15,
+        paddingBottom: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#a9a9a9',
+        backgroundColor: '#ffaf3f'
+  },
+  content: {
+        paddingTop: 15,
+        paddingRight: 15,
+        paddingBottom: 15,
+        paddingLeft: 15,
+        color: '#000',
+  },
+  text: {
+    flex: 1,
+  },
+});
