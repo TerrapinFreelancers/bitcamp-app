d5cd4ee03912e378326b3421976610adaa9d1772
jsjqian <jack.qian97@gmail.com>
2017-01-24 15:26:25 -0500

    Change the map orientation and refactor the MapScene

--- scenes/MapScene.js
+++ scenes/MapScene.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, Image, Dimensions } from 'react-native';
 
-export default class MapScene extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    var window = Dimensions.get('window');
-    return (
+function MapScene() {
+  var window = Dimensions.get('window');
+  return (
+    <ScrollView directionLockEnabled={false}
+                horizontal={true}>
       <ScrollView>
         <Image 
           source={require('./images/floor_plan_2017.png')} 
-          style={{
-            width: window.width,
-            height: window.height
-          }}
         />
       </ScrollView>
-    )
-  }
+    </ScrollView>
+  )
 }
+
+export default MapScene
