8351354ea1b85d7bf8491d61eff26753b1614b82
George <gtong@terpmail.umd.edu>
2017-03-05 15:15:17 -0500

    modify ios settings so lock orientation works with ios.
    specifically adds header search paths in xcode so project can find orientation library files at compile time.

--- ios/bitcampapp/AppDelegate.m
+++ ios/bitcampapp/AppDelegate.m
@@ -27,16 +27,16 @@
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
-  - (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
-    return [Orientation getOrientation];
-  }
-
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+}
+
+- (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
+  return [Orientation getOrientation];
 }
 
 @end
