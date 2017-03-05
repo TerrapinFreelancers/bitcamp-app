f7d8e293139c31023b13883d5da24c9352983276
jsjqian <jack.qian97@gmail.com>
2017-03-05 02:07:16 -0500

    Remove extraneous features

--- scenes/MentorsScene.js
+++ scenes/MentorsScene.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, WebView, TouchableWithoutFeedback, InteractionManager,
-  TouchableOpacity, StyleSheet } from 'react-native';
+import { View, WebView, StyleSheet } from 'react-native';
 import { colors } from '../shared/styles';
 
 const DEFAULT_URL = 'https://mentorship.bitca.mp';
@@ -9,45 +8,15 @@ export default class MentorsScene extends Component {
   state = {
     url: DEFAULT_URL,
     status: 'No Page Loaded',
-    backButtonEnabled: false,
-    forwardButtonEnabled: false,
     loading: true,
     scalesPageToFit: true,
-    renderPlaceholderOnly: true,
   };
 
-  componentDidMount() {
-    InteractionManager.runAfterInteractions(() => {
-      this.setState({renderPlaceholderOnly: false});
-    });
-  }
-
   render() {
-    if (this.state.renderPlaceholderOnly) {
-      return this._renderPlaceholderView();
-    }
 
     return (
       <View style={styles.container}>
-        <View style={styles.addressBarRow}>
-          <TouchableOpacity
-            onPress={this.goBack}
-            style={this.state.backButtonEnabled ? styles.navButton : 
-            styles.disabledButton}>
-            <Text>
-               {'<'}
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={this.goForward}
-            style={this.state.forwardButtonEnabled ? styles.navButton : 
-            styles.disabledButton}>
-            <Text>
-              {'>'}
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <WebView
+       <WebView
           ref={(view) => this.webview = view}
           automaticallyAdjustContentInsets={false}
           style={styles.webView}
@@ -60,40 +29,12 @@ export default class MentorsScene extends Component {
           startInLoadingState={true}
           scalesPageToFit={this.state.scalesPageToFit}
         />
-        <View style={styles.statusBar}>
-          <Text style={styles.statusBarText}>{this.state.status}</Text>
-        </View>
       </View>
     );
   }
-
-  _renderPlaceholderView() {
-    return (
-      <View style={styles.container}>
-      </View>
-    );
-  }
-
-  goBack = () => {
-    this.webview.goBack();
-  };
-
-  goForward = () => {
-    this.webview.goForward();
-  };
 
   onShouldStartLoadWithRequest = (event) => {
     return true;
-  };
-
-  onNavigationStateChange = (navState) => {
-    this.setState({
-      backButtonEnabled: navState.canGoBack,
-      forwardButtonEnabled: navState.canGoForward,
-      status: navState.title,
-      loading: navState.loading,
-      scalesPageToFit: true
-    });
   };
 
 }
@@ -103,83 +44,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.midnightBLue,
   },
-  addressBarRow: {
-    flexDirection: 'row',
-    padding: 8,
-  },
   webView: {
     backgroundColor: colors.BGWASH,
     height: 350,
-  },
-  addressBarTextInput: {
-    backgroundColor: colors.BGWASH,
-    borderColor: 'transparent',
-    borderRadius: 3,
-    borderWidth: 1,
-    height: 24,
-    paddingLeft: 10,
-    paddingTop: 3,
-    paddingBottom: 3,
-    flex: 1,
-    fontSize: 14,
-  },
-  navButton: {
-    width: 20,
-    padding: 3,
-    marginRight: 3,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: colors.BGWASH,
-    borderColor: 'transparent',
-    borderRadius: 3,
-  },
-  disabledButton: {
-    width: 20,
-    padding: 3,
-    marginRight: 3,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: colors.DISABLED_WASH,
-    borderColor: 'transparent',
-    borderRadius: 3,
-  },
-  goButton: {
-    height: 24,
-    padding: 3,
-    marginLeft: 8,
-    alignItems: 'center',
-    backgroundColor: colors.BGWASH,
-    borderColor: 'transparent',
-    borderRadius: 3,
-    alignSelf: 'stretch',
-  },
-  statusBar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingLeft: 5,
-    height: 22,
-  },
-  statusBarText: {
-    color: 'white',
-    fontSize: 13,
-  },
-  spinner: {
-    width: 20,
-    marginRight: 6,
-  },
-  buttons: {
-    flexDirection: 'row',
-    height: 30,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  button: {
-    flex: 0.5,
-    width: 0,
-    margin: 5,
-    borderColor: 'gray',
-    borderWidth: 1,
-    backgroundColor: 'gray',
   },
 });
