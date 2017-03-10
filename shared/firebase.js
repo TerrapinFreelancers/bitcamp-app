54798beaf58d29af0e1d835af08c45135a73a927
jsjqian <jack.qian97@gmail.com>
2017-03-09 22:08:26 -0500

    Update with new account

--- shared/firebase.js
+++ shared/firebase.js
@@ -1,10 +1,10 @@
 import * as firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDBhF1-PpgXUc2EA-ro4j0-XJq3zkf68Gw",
-    authDomain: "bitcamp-2017.firebaseapp.com",
-    databaseURL: "https://bitcamp-2017.firebaseio.com",
-    storageBucket: "bitcamp-2017.appspot.com"
+    apiKey: "AIzaSyC98AMk0_v5qXGDmK9VvPvmfwJBrxOKXLI",
+    authDomain: "bitcamp-2017-761ae.firebaseapp.com",
+    databaseURL: "https://bitcamp-2017-761ae.firebaseio.com",
+    storageBucket: "bitcamp-2017-761ae.appspot.com"
 };
 firebaseApp = firebase.initializeApp(firebaseConfig);
 module.exports = firebaseApp;
