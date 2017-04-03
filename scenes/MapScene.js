845eea01c56bcfdfb5198c52a8608f901cf42128
George <gtong@terpmail.umd.edu>
2017-04-02 22:17:47 -0400

    bold front bitcamp image and add zoom to maps

--- scenes/MapScene.js
+++ scenes/MapScene.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import { View, ScrollView, Text, Image, Dimensions, Platform } from 'react-native';
+import { View, ScrollView, Text, Image, Dimensions } from 'react-native';
 import PhotoView from 'react-native-photo-view';
 
 const window = Dimensions.get('window');
 
 function MapScene() {
-  let styles = {};
-  if (Platform.OS === 'android'){
-    styles = {width: window.width, height: window.height}
-  }
+  let styles = {width: window.width, height: window.height, overflow:'visible'};
+
   return(
     <View>
       <PhotoView
-        source={require('./images/floor_plan_2017.png')}
+        source={require('./images/floor_plan_final.png')}
         minimumZoomScale={0.5}
         maximumZoomScale={3}
-        androidScaleType="center"
+        androidScaleType="fitCenter"
         onLoad={() => console.log("Image loaded!")}
         style={styles} />
     </View>
